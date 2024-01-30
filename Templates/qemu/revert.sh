0083f52f46941ac9ee69a75379107dd34c2b98f3
Samuil Petrov <samuil.petrov@maxe-del.de>
2024-01-30 20:58:30 +0100

    Appending gaming VM scripts

---
+++ Templates/qemu/revert.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Re-enables all cores dynamically
+systemctl set-property --runtime -- user.slice AllowedCPUs=0-15
+systemctl set-property --runtime -- system.slice AllowedCPUs=0-15
+systemctl set-property --runtime -- init.scope AllowedCPUs=0-15
+
+# Unload the VMs VFIO-PCI drivers
+modprobe -r vfio_pci
+modprobe -r vfio_iommu_type1
+modprobe -r vfio
+
+# Avoid race conditions
+sleep 2
+
+# Re-attaches the GPU to the host
+virsh nodedev-reattach pci_0000_08_00_1
+virsh nodedev-reattach pci_0000_08_00_0
+
+# Loads the NVIDIA drivers
+modprobe drm
+modprobe drm_kms_helper
+modprobe i2c_nvidia_gpu
+modprobe nvidia
+modprobe nvidia_modeset
+modprobe nvidia_drm
+modprobe nvidia_uvm
+
+# Rebinds the EFI frame buffer to the GPU
+echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/bind
+
+# Rebinds the TTYs
+input="/tmp/vfio-bound-consoles"
+while read -r consoleNumber; do
+    if test -x /sys/class/vtconsole/vtcon"${consoleNumber}"; then
+        if [ "$(grep -c "frame buffer" "/sys/class/vtconsole/vtcon${consoleNumber}/name")" = 1 ]; then
+	    echo 1 > /sys/class/vtconsole/vtcon"${consoleNumber}"/bind
+        fi
+    fi
+done < "$input"
+
+# Starts Xorg again
+systemctl start sddm.service
+
+# Lowering power consumption by resetting frequency governer
+#for file in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do
+#	echo "powersave" > $file
+#done
---
+++ Templates/qemu/start.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Improving performance by fixing CPU frequency scaling governer
+#for file in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do
+#	echo "performance" > $file
+#done
+
+# Stops the display manager (Xorg)
+systemctl stop sddm.service
+
+# Unbinds TTYs
+for (( i = 0; i < 7; i++)); do
+  if test -x /sys/class/vtconsole/vtcon"${i}"; then
+      if [ "$(grep -c "frame buffer" /sys/class/vtconsole/vtcon"${i}"/name)" = 1 ]; then
+	  echo 0 > /sys/class/vtconsole/vtcon"${i}"/bind
+          echo "$i" >> /tmp/vfio-bound-consoles
+      fi
+  fi
+done
+
+# Unbinds the GPUs EFI frame buffer
+echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
+
+# Unloads the NVIDIA drivers
+modprobe -r nvidia_uvm
+modprobe -r nvidia_drm
+modprobe -r nvidia_modeset
+modprobe -r nvidia
+modprobe -r i2c_nvidia_gpu
+modprobe -r drm_kms_helper
+modprobe -r drm
+
+# Cleans up RAM for THP allocation
+echo 3 > /proc/sys/vm/drop_caches
+echo 1 > /proc/sys/vm/compact_memory
+
+# Avoid race conditions
+sleep 2
+
+# Unbind the GPU from display driver
+virsh nodedev-detach pci_0000_08_00_0
+virsh nodedev-detach pci_0000_08_00_1
+
+# Loads the VMs VFIO-PCI drivers
+modprobe vfio
+modprobe vfio_pci
+modprobe vfio_iommu_type1
+
+# Isolates VM's cores dynamically
+systemctl set-property --runtime -- user.slice AllowedCPUs=0-1,8-9
+systemctl set-property --runtime -- system.slice AllowedCPUs=0-1,8-9
+systemctl set-property --runtime -- init.scope AllowedCPUs=0-1,8-9
