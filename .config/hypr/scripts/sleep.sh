4fe88aa8cb57d24474d0a43304f870ef383f9057
Samuil1337 <130246533+Samuil1337@users.noreply.github.com>
2024-05-20 08:09:51 +0000

    Delete sleep.sh because of switch to hypridle

--- .config/hypr/scripts/sleep.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-swayidle -w timeout 600 'hyprctl dispatch dpms off' \
-	    resume 'hyprctl dispatch dpms on' \
-            timeout 900 'swaylock -f -c 000000' \
-            before-sleep 'swaylock -f -c 000000' &
